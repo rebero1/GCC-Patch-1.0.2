5df2c55057297252ba63a4564c33e425d07152cd
Prince <reberoprince@gmail.com>
2019-10-23 00:08:39 -0400

    Update Question6.py

--- python/Question6.py
+++ python/Question6.py
@@ -1,5 +1,21 @@
 # modify this function, and create other functions below as you wish
 def question06(portfolio):
     # modify and then return the variable below
-    answer = -5
-    return answer
+    first_answer = len(portfolio[0])
+       
+        start = portfolio[0][0]
+        ends   = portfolio[0][first_answer-1]
+         
+
+        answer =  len(portfolio)
+        portfolio.reverse()
+
+        for data in portfolio:
+            data =str(data)
+            if (data.startswith(start)) and data.endswith(ends):
+                if answer-1 == 0:
+                    return -1
+                else:
+                    return (answer  -1)
+            answer = answer - 1
+    
